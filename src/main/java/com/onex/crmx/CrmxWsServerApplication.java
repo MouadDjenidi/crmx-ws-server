a63889780547e378280f055d4e5a72528cba5d93
Mouad Errafik Djenidi <mdj@audaxis.com>
2022-04-24 01:11:05 +0200

    first commit

---
+++ src/main/java/com/onex/crmx/CrmxWsServerApplication.java
@@ -0,0 +1,13 @@
+package com.onex.crmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmxWsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmxWsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onex/crmx/CrmxWsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onex.crmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmxWsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
